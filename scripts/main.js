604994deb015ad0ad59f5a56bd7cd803854ef82f
Leslie Yacyac <leslievhieryacyac@gmail.com>
2025-03-04 19:52:14 -0800

    added a card template

--- scripts/main.js
+++ scripts/main.js
@@ -8,7 +8,7 @@ function getNameFromAuth() {
             userName = user.displayName;
 
             //method #1:  insert with JS
-            document.getElementById("name-goes-here").innerText = userName;    
+            document.getElementById("name-goes-here").innerText = userName;
 
             //method #2:  insert using jquery
             //$("#name-goes-here").text(userName); //using jquery
@@ -18,7 +18,7 @@ function getNameFromAuth() {
 
         } else {
             // No user is signed in.
-            console.log ("No user is logged in");
+            console.log("No user is logged in");
         }
     });
 }
@@ -38,7 +38,81 @@ function readQuote(day) {
             //document.querySelector("#quote-goes-here").innerHTML = dayDoc.data().quote;
 
         }, (error) => {
-            console.log ("Error calling onSnapshot", error);
+            console.log("Error calling onSnapshot", error);
         });
-    }
- readQuote("tuesday");        //calling the function
+}
+readQuote("tuesday");        //calling the function
+
+function writeHikes() {
+    //define a variable for the collection you want to create in Firestore to populate data
+    var hikesRef = db.collection("hikes");
+
+    hikesRef.add({
+        code: "BBY01",
+        name: "Burnaby Lake Park Trail", //replace with your own city?
+        city: "Burnaby",
+        province: "BC",
+        level: "easy",
+        details: "A lovely place for lunch walk",
+        length: 10,          //number value
+        hike_time: 60,       //number value
+        lat: 49.2467097082573,
+        lng: -122.9187029619698,
+        image: "./images/BBY01.jpg",
+        last_updated: firebase.firestore.FieldValue.serverTimestamp()  //current system time
+    });
+    hikesRef.add({
+        code: "AM01",
+        name: "Buntzen Lake Trail", //replace with your own city?
+        city: "Anmore",
+        province: "BC",
+        level: "moderate",
+        details: "Close to town, and relaxing",
+        length: 10.5,      //number value
+        hike_time: 80,     //number value
+        lat: 49.3399431028579,
+        lng: -122.85908496766939,
+        image: "./images/AM01.jpg",
+        last_updated: firebase.firestore.Timestamp.fromDate(new Date("March 10, 2022"))
+    });
+    hikesRef.add({
+        code: "NV01",
+        name: "Mount Seymour Trail", //replace with your own city?
+        city: "North Vancouver",
+        province: "BC",
+        level: "hard",
+        details: "Amazing ski slope views",
+        length: 8.2,        //number value
+        hike_time: 120,     //number value
+        lat: 49.38847101455571,
+        lng: -122.94092543551031,
+        image: "./images/NV01.jpg",
+        last_updated: firebase.firestore.Timestamp.fromDate(new Date("January 1, 2023"))
+    });
+}
+writeHikes();         //if you used method 1
+
+function displayCardsDynamically(collection) {
+    let cardTemplate = document.getElementById("hikeCardTemplate");
+
+    db.collection(collection).get()
+    .then(allHikes => {
+        allHikes.forEach(doc => {
+            var title = doc.data().name;
+            var details = doc.data().details;
+            var hikeCode = doc.data().code;
+            var hikeLength = doc.data().length;
+            var docID = doc.id;
+            let newcard = cardTemplate.contentEditable.cloneNode(true);
+
+            newcard.querySelector('.card-title').innerHTML = title;
+            newcard.querySelector('.card-length').innerHTML = hikeLength + "km";
+            newcard.querySelector('.card-text').innerHTML = details;
+            newcard.querySelector('.card-image').src = `./images/${hikeCode}.jpg`;
+            newcard.querySelector('a').href = "eachHike.html?docID=" + docID;
+
+            document.getElementById(collection + "-go-here").appendChild(newcard);
+        })
+    })
+}
+displayCardsDynamically();
